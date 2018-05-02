adf80fb856970d40bb9f78d078b1297dd75d672f
erfantkerfan <erfantkerfan@yahoo.com>
2018-05-02 20:06:38 +0430

    change in nav bar non static

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,126 +55,125 @@
 
 </head>
 <body style="font-family:'Font'" class="bg">
-    <div id="app">
-        <nav class="navbar navbar-inverse navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+    <nav class="navbar navbar-default"></nav>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name') }}
-                    </a>
-                </div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name') }}
+                </a>
+            </div>
 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
 
-                    </ul>
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        @guest
-                            <li><a href="{{ route('login') }}">ورود</a></li>
-                        @else
-                            @if (Auth::user()->is_admin==1)
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                        ابزارها <span class="caret"></span>
-                                    </a>
-                                <ul class="dropdown-menu">
-
-                                    <li class="text-center">
-
-                                        <a href="{{ route('notification') }}">اعلان ها</a>
-
-                                        <a href="{{ route('expense') }}">هزینه های صندوق</a>
-
-                                        <a href="{{ route('register') }}">ثبت اطلاعات عضو</a>
-
-                                        <a href="{{ Storage::url('public/Mysql_Backup_Ghaem.sql') }}" onclick="return confirm('آیا از دانلود دیتابیس اطمینان دارید؟')" >
-                                            <span class="glyphicon glyphicon-download-alt"></span>
-                                            دانلود دیتابیس
-                                        </a>
-
-                                    </li>
-                                </ul>
-
-                                <li>
-                                    <a href="{{ route('notes') }}">
-                                        <div class="badge">
-                                            {{App\User::where('note','!=',null)->count()+App\User::where('user_note','!=',null)->count()}}
-                                        </div>
-                                        پیام ها
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="{{ route('instalment') }}">
-                                        <div class="badge">
-                                            {{App\User::where('instalment','!=',null)->count()+App\User::where('instalment_force','!=',null)->count()}}
-                                        </div>
-                                        اقساط فعال
-                                    </a>
-                                </li>
-
-                                <li>
-                                    <a href="{{ route('unproved') }}">
-                                        <div class="badge">
-                                            {{App\Payment::where('is_proved','=','0')->count()+App\Loan::where('is_proved','=','0')->count()}}
-                                        </div>
-                                        تایید تراکنش
-                                    </a>
-                                </li>
-
-                                <li><a href="{{ route('admin') }}">صفحه مدیریت</a></li>
-                            @endif
-
-
-                            <li> <a href="{{ route('home') }}">تراکنش ها</a></li>
-
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    @guest
+                        <li><a href="{{ route('login') }}">ورود</a></li>
+                    @else
+                        @if (Auth::user()->is_admin==1)
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->username }} <span class="caret"></span>
+                                    ابزارها <span class="caret"></span>
                                 </a>
-                                <ul class="dropdown-menu">
-                                    <li class="text-center">
+                            <ul class="dropdown-menu">
 
-                                        <a href="#"><span class="glyphicon glyphicon-time"></span>{{str_before(\Hekmatinasser\Verta\Verta::now(),' ')}}</a>
+                                <li class="text-center">
 
-                                        <a href="#"><span class="glyphicon glyphicon-map-marker"></span>{{Request::getClientIp()}}</a>
+                                    <a href="{{ route('notification') }}">اعلان ها</a>
 
-                                        <a style="color: red" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                       document.getElementById('logout-form').submit();">
-                                            <span class="glyphicon glyphicon-log-out"></span>
-                                            خروج
-                                                </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                        </form>
+                                    <a href="{{ route('expense') }}">هزینه های صندوق</a>
 
-                                    </li>
-                                </ul>
+                                    <a href="{{ route('register') }}">ثبت اطلاعات عضو</a>
+
+                                    <a href="{{ Storage::url('public/Mysql_Backup_Ghaem.sql') }}" onclick="return confirm('آیا از دانلود دیتابیس اطمینان دارید؟')" >
+                                        <span class="glyphicon glyphicon-download-alt"></span>
+                                        دانلود دیتابیس
+                                    </a>
+
+                                </li>
+                            </ul>
+
+                            <li>
+                                <a href="{{ route('notes') }}">
+                                    <div class="badge">
+                                        {{App\User::where('note','!=',null)->count()+App\User::where('user_note','!=',null)->count()}}
+                                    </div>
+                                    پیام ها
+                                </a>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
+                            <li>
+                                <a href="{{ route('instalment') }}">
+                                    <div class="badge">
+                                        {{App\User::where('instalment','!=',null)->count()+App\User::where('instalment_force','!=',null)->count()}}
+                                    </div>
+                                    اقساط فعال
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="{{ route('unproved') }}">
+                                    <div class="badge">
+                                        {{App\Payment::where('is_proved','=','0')->count()+App\Loan::where('is_proved','=','0')->count()}}
+                                    </div>
+                                    تایید تراکنش
+                                </a>
+                            </li>
+
+                            <li><a href="{{ route('admin') }}">صفحه مدیریت</a></li>
+                        @endif
+
+
+                        <li> <a href="{{ route('home') }}">تراکنش ها</a></li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                {{ Auth::user()->username }} <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="text-center">
+
+                                    <a href="#"><span class="glyphicon glyphicon-time"></span>{{str_before(\Hekmatinasser\Verta\Verta::now(),' ')}}</a>
+
+                                    <a href="#"><span class="glyphicon glyphicon-map-marker"></span>{{Request::getClientIp()}}</a>
+
+                                    <a style="color: red" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                                        <span class="glyphicon glyphicon-log-out"></span>
+                                        خروج
+                                            </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                    </form>
+
+                                </li>
+                            </ul>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
         @yield('content')
     <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
 
-</div>
 </body>
 </html>

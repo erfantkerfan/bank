d40ffe0ce6d6f77958a3d7a3947eaaa3baedc80b
unknown <erfantkerfan@yahoo.com>
2018-10-26 17:57:54 +0330

    verify method correction 3

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -161,6 +161,7 @@ class PaymentController extends Controller
         if ($_GET['Status'] == 'OK') {
             $Authority = $_GET['Authority'];
             $onlinepayment = Onlinepayment::where('authority','=',$Authority)->get();
+            dd($onlinepayment);
             $result = Zarinpal::verify('OK',($onlinepayment->amount)/10,$onlinepayment->authority);
             if ($result->Status == 100) {
                 $onlinepayment->refid = $result->RefID;

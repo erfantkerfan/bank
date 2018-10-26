50780cee7066d0c74f89f473e982cf1efaba5abe
unknown <erfantkerfan@yahoo.com>
2018-10-26 18:20:00 +0330

    verify method correction 13

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -170,6 +170,7 @@ class PaymentController extends Controller
                 $payment->is_proved=1;
                 $payment->proved_by=$result->RefID;
                 $payment->save();
+                redirect(route('home'));
             } else {
                 return view('errors.payment');
             }

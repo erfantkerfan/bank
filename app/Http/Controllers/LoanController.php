b972fac08257d4ef26260823a54f732386e5c46d
Ali Yousefi <ayousefi@outlook.com>
2020-08-16 16:01:12 +0430

    clean up and refactor

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Loan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,62 +13,61 @@ class LoanController extends Controller
         $loan = Loan::query()->findOrFail($id);
         $loan->is_proved = 1;
         $loan->proved_by = auth()->user()->l_name;
-        $loan-> save();
+        $loan->save();
 
-        return redirect()->route('user_edit',['id'=>$loan->user->id]);
+        return redirect()->route('user_edit', ['id' => $loan->user->id]);
     }
 
     public function delete($id)
     {
         $loan = Loan::query()->findOrFail($id);
-        if (Auth::user()->is_super_admin == 1 || ($loan->user_id==Auth::user()->id && $loan->isproved==0)) {
-            $loan->delete();
-            return redirect()->back();
-        } else {
+        if (auth()->user()->is_super_admin != 1 && ($loan->user_id != auth()->id() || $loan->isproved != 0)) {
             abort(403);
         }
+        $loan->delete();
+        return redirect()->back();
     }
 
     public function forcedelete($id)
     {
         $loan = Loan::onlyTrashed()->FindOrFail($id);
-        if (Auth::user()->is_super_admin == 1) {
-            $loan->forceDelete();
-            return redirect()->back();
-        } else {
+        if (auth()->user()->is_super_admin != 1) {
             abort(403);
         }
+        $loan->forceDelete();
+        return redirect()->back();
     }
 
     public function create(request $request)
     {
         $input = $request->all();
-        if($input["loan"]!=null){$input["loan"] = str_replace(",","",$input["loan"]);}
+        if ($input["loan"] != null) {
+            $input["loan"] = str_replace(",", "", $input["loan"]);
+        }
         $request->replace((array)$input);
 
         $proved_by = null;
-        if($request->is_proved==1){
+        if ($request->is_proved == 1) {
             $proved_by = auth()->user()->l_name;
         };
 
         $user_id = basename(url()->previous());
 
         if (($user_id) == 'home') {
-            $user_id = Auth::user()->id;
+            $user_id = auth()->id();
         }
 
-        if (Auth::user()->is_super_admin == 0 && Auth::user()->id != $user_id) {
+        if (auth()->user()->is_super_admin == 0 && auth()->id() != $user_id) {
             abort(500);
         };
 
-        if(($request->has('is_proved'))){
+        if (($request->has('is_proved'))) {
             $is_proved = $request->is_proved;
-        }
-        else{
-            $is_proved=0;
+        } else {
+            $is_proved = 0;
         }
 
-        $creator = auth()->user()->f_name.' '.auth()->user()->l_name;
+        $creator = auth()->user()->f_name . ' ' . auth()->user()->l_name;
 
         $date_time = verta();
 
@@ -98,22 +98,22 @@ class LoanController extends Controller
     public function show_edit($id)
     {
         $loan = Loan::query()->findOrFail($id);
-        return view('loan_edit')->with(['loan'=>$loan]);
+        return view('loan_edit')->with(['loan' => $loan]);
     }
 
-    public function edit(request $request , $id)
+    public function edit(request $request, $id)
     {
         $loan = Loan::query()->findOrFail($id);
 
-        if ($loan->is_proved==1){
-            $loan->proved_by = auth()->user()->f_name.' '.auth()->user()->l_name;
+        if ($loan->is_proved == 1) {
+            $loan->proved_by = auth()->user()->f_name . ' ' . auth()->user()->l_name;
         }
 
         $input = $request->all();
         if ($input["loan"] != null) {
             $input["loan"] = str_replace(",", "", $input["loan"]);
         }
-        $request->replace((array) $input);
+        $request->replace((array)$input);
 
         $this->Validate($request, [
             'loan' => 'nullable|integer',
@@ -130,6 +130,6 @@ class LoanController extends Controller
 
         $loan->save();
 
-        return redirect(route('user',['id'=>$loan->user_id]));
+        return redirect(route('user', ['id' => $loan->user_id]));
     }
 }

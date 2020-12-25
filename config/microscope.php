e0f7aab560e4050424b4bfca053e931bc5aa14a0
Erfan Gholizade <erfantkerfan@yahoo.com>
2020-12-26 00:14:30 +0330

    microscope config

---
+++ config/microscope.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /**
+     * An array of patterns relative to base_path that should be ignored when reporting.
+     */
+    'ignore' => [
+        // 'nova*'
+    ],
+
+    /**
+     * You can turn off the extra variable passing detection, which performs some logs.
+     */
+    'log_unused_view_vars' => true,
+
+    /**
+     * An array of root namespaces to be ignored while scanning for errors.
+     */
+    'ignored_namespaces' => [
+        // 'Laravel\\Nova\\',
+        // 'Laravel\\Nova\\Tests\\'
+    ],
+];

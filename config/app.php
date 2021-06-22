9edc9139decc4c1ef221675993915f4d27bb9b6a
Erfan <37825504+erfantkerfan@users.noreply.github.com>
2021-06-22 21:28:03 +0430

    upgrade to laravel 7.x

--- config/app.php
+++ config/app.php
@@ -164,7 +164,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         //
-        Hekmatinasser\Verta\VertaServiceProvider::class,
         Backup\BackupServiceProvider::class,
         niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
         Zarinpal\Laravel\ZarinpalServiceProvider::class,

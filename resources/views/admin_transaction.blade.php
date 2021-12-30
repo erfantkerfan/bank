baa04a9a15394aefc6cf66270351644b04ea5899
Erfan <37825504+erfantkerfan@users.noreply.github.com>
2021-12-30 09:38:24 +0330

    Update admin_transaction.blade.php

--- resources/views/admin_transaction.blade.php
+++ resources/views/admin_transaction.blade.php
@@ -203,7 +203,6 @@
                             @endforeach
                             </tbody>
                         </table>
-                        {{ $users->links() }}
                     </div>
                 </div>
             </div>

5836592703fd74274c62ccdffdc6a006a430b253
Erfan Gholizade <erfantkerfan@yahoo.com>
2022-08-06 11:36:00 +0430

    rename destinations folders

--- docker/create_service.sh
+++ docker/create_service.sh
@@ -6,11 +6,11 @@ docker service create \
     --publish published=81,target=80,mode=ingress \
     --with-registry-auth \
     --init \
-    --mount type=bind,source=/var/log/direct_debit/.env,destination=/var/www/html/.envno \
-    --mount type=bind,source=/var/log/direct_debit/nginx,destination=/var/log/nginx \
-    --mount type=bind,source=/var/log/direct_debit/php,destination=/var/log/php \
-    --mount type=bind,source=/var/log/direct_debit/supervisor,destination=/var/log/supervisor \
-    --mount type=bind,source=/var/log/direct_debit/laravel_storage,destination=/var/www/html/storage \
+    --mount type=bind,source=/var/log/ghaem/.env,destination=/var/www/html/.env \
+    --mount type=bind,source=/var/log/ghaem/nginx,destination=/var/log/nginx \
+    --mount type=bind,source=/var/log/ghaem/php,destination=/var/log/php \
+    --mount type=bind,source=/var/log/ghaem/supervisor,destination=/var/log/supervisor \
+    --mount type=bind,source=/var/log/ghaem/laravel_storage,destination=/var/www/html/storage \
     --mount type=tmpfs,destination=/var/nginx/cache,tmpfs-size=10m \
     -u nobody \
     $1

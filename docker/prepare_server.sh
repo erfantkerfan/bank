93ae8600d0bdf208a9c87063e72396b5ce1e704c
erfan gholizade <erfantkerfan@yahoo.com>
2022-08-12 23:13:44 +0430

    remove unnecessary folde creation

--- docker/prepare_server.sh
+++ docker/prepare_server.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-mkdir -p /var/log/ghaem/laravel_storage/logs
-mkdir -p /var/log/ghaem/laravel_storage/queue_logs
-mkdir -p /var/log/ghaem/laravel_storage/framework/cache
-mkdir -p /var/log/ghaem/laravel_storage/framework/sessions
-mkdir -p /var/log/ghaem/laravel_storage/framework/views
+mkdir -p /var/log/ghaem/laravel_storage
 
 mkdir -p /var/log/ghaem/nginx
 mkdir -p /var/log/ghaem/php

717baf5c87fc3664be861b9f8eeb7f9d3d8fa1c2
Erfan Gholizade <erfantkerfan@yahoo.com>
2023-06-02 08:09:29 +0330

    improve production event cache

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd /var/www/html/ && php artisan optimize && php artisan cache:clear
+cd /var/www/html/ && php artisan event:cache && php artisan optimize && php artisan cache:clear
 supervisord -c /etc/supervisord.conf
 exec nginx

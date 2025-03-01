06d9dde060c50900fbed8742cdad011ab74895ea
Erfan Gholizade <erfantkerfan@yahoo.com>
2025-03-01 17:18:45 +0330

    rename gif to have no spaces in name

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -6,7 +6,7 @@
     <div class="row title m-b-md">
         سایت در حال بروزرسانی یا تعمیر می باشد
         <br>
-        <img class="col-md-12 row" src="{{asset('img/maintenance boy.gif')}}" style="width: 60%;float: none">
+        <img class="col-md-12 row" src="{{asset('img/maintenance_boy.gif')}}" style="width: 60%;float: none">
         <br>
     </div>
     <div class="row number font-red">503</div>
--- resources/views/errors/payment.blade.php
+++ resources/views/errors/payment.blade.php
@@ -6,7 +6,7 @@
     <div class="row title m-b-md">
         پرداخت با خطا مواجه شد
         <br>
-        <img class="col-md-12 row" src="{{asset('img/maintenance boy.gif')}}" style="width: 60%;float: none">
+        <img class="col-md-12 row" src="{{asset('img/maintenance_boy.gif')}}" style="width: 60%;float: none">
         <br>
     </div>
     <div class="row number font-red">999</div>
